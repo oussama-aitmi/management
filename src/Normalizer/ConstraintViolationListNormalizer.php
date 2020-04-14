2875cfc5b5e08e01c290516a6449357d1f0e1b2e
Oussama <oussama.aitmi@gmail.com>
2020-04-14 19:34:41 +0100

    Normalizer to format violation messages

---
+++ src/Normalizer/ConstraintViolationListNormalizer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Normalizer;
+
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+/**
+ * Class ConstraintViolationListNormalizer
+ * @package App\Infrastructure\Normalizer
+ */
+final class ConstraintViolationListNormalizer implements NormalizerInterface
+{
+
+    /**
+     * @param object $object
+     * @param null $format
+     * @param array $context
+     * @return array
+     */
+    public function normalize($object, $format = null, array $context = array()): array
+    {
+
+        [$messages, $violations] = $this->getMessagesAndViolations($object);
+
+        return [
+            "status"=> "error",
+            'code' => 400,
+            'title' => $context['title'] ?? 'An error occurred',
+            'message' => $messages ? implode("\n", $messages) : '',
+            'error' => $violations,
+        ];
+    }
+
+    /**
+     * @param ConstraintViolationListInterface $constraintViolationList
+     * @return array
+     */
+    private function getMessagesAndViolations(ConstraintViolationListInterface $constraintViolationList): array
+    {
+        $violations = $messages = [];
+        /** @var ConstraintViolation $violation */
+        foreach ($constraintViolationList as $violation) {
+            $violations[] = [
+                'propertyPath' => $violation->getPropertyPath(),
+                'message' => $violation->getMessage(),
+                //'code' => $violation->getCode(),
+            ];
+
+            $propertyPath = $violation->getPropertyPath();
+            $messages[] = ($propertyPath ? $propertyPath.': ' : '').$violation->getMessage();
+        }
+
+        return [$messages, $violations];
+    }
+
+    /**
+     * @param mixed $data
+     * @param null $format
+     * @return bool
+     */
+    public function supportsNormalization($data, $format = null): bool
+    {
+        return $data instanceof ConstraintViolationListInterface;
+    }
+}
