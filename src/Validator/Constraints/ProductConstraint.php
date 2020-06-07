7ee6eff15cce370eb82abf27e788d402565fa3df
Oussama <oussama.aitmi@gmail.com>
2020-06-07 01:28:19 +0100

    Product Validator Service

---
+++ src/Validator/Constraints/ProductConstraint.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Validator\Constraints;
+
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class ProductConstraint extends Constraint
+{
+
+    /**
+     * Returns the name of the class that validates this constraint.
+     *
+     * By default, this is the fully qualified name of the constraint class
+     * suffixed with "Validator". You can override this method to change that
+     * behaviour.
+     *
+     * @return string
+     */
+    public function validatedBy()
+    {
+        return 'App\Validator\ProductValidator';
+    }
+
+
+    /**
+     * Returns whether the constraint can be put onto classes, properties or
+     * both.
+     *
+     * This method should return one or more of the constants
+     * Constraint::CLASS_CONSTRAINT and Constraint::PROPERTY_CONSTRAINT.
+     *
+     * @return string|array One or more constant values
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
---
+++ src/Validator/ProductValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Validator;
+
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+
+class ProductValidator extends ConstraintValidator
+{
+    /**
+     * @var CategoryRepository
+     */
+    private $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @param mixed      $value
+     * @param Constraint $constraint
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        $this->validateCategory($value);
+    }
+
+    protected function validateCategory(Product $product)
+    {
+        if (!$category = $this->categoryRepository->findOneBy(['id'=> $product->getCategory()])) {
+            $this->context->buildViolation('Catégorie est invalide')
+                ->atPath('category')
+                ->setInvalidValue(null)
+                ->addViolation();
+        }
+    }
+}
