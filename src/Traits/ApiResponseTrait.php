1ee6e6ab6b68aaa6b3f276d39a5265fc675ecfd4
Oussama <oussama.aitmi@gmail.com>
2020-06-08 04:22:49 +0100

    Clean Up

--- src/Traits/ApiResponseTrait.php
+++ src/Traits/ApiResponseTrait.php
@@ -25,6 +25,69 @@ trait ApiResponseTrait
     }
 
     /**
+     * @param  $violations
+     * @param null $format
+     * @param array $context
+     * @return array
+     */
+    public function normalizeViolations($violations, $format = null)
+    {
+        //[$messages, $violations] = $this->getMessagesAndViolations($object);
+        //dd($violations);
+        return [
+            //'message' => $messages ? implode("\n", $messages) : 'Une erreur est survenue',
+            'form2222' => $violations,
+        ];
+    }
+
+    /**
+     * @param $constraintViolationList
+     * @return array
+     */
+    public function getMessagesAndViolations($constraintViolationList): array
+    {
+        $violations = $messages = [];
+
+        foreach ($constraintViolationList as $violation) {
+            $violations[] = [
+                $violation->getPropertyPath() => $violation->getMessage() ,
+            ];
+
+            $propertyPath = $violation->getPropertyPath();
+            //$messages[] = ($propertyPath ? $propertyPath.': ' : '').$violation->getMessage();
+            $messages[] = $violation->getMessage();
+
+        }
+
+        return array_filter([
+            //'message' => $messages ?: implode("\n ", $messages),
+            'errors' => $violations,
+        ]);
+
+    }
+
+    /**
+     * @param $constraintViolationList
+     * @return array|\array[][]
+     */
+    public function getDetailsViolations($constraintViolationList): array
+    {
+        $violations = $messages = [];
+
+        foreach ($constraintViolationList as $violation) {
+            $violations[$violation->getPropertyPath()] = $violation->getMessage();
+        }
+
+        if(!$violations){
+            return [];
+        }
+
+        return array_filter([
+            'detail' => [$violations]
+        ]);
+    }
+
+    /**
      * @ return array
      *
     protected function renderSuccessPostDeleted()
