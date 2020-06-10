8032ca05e229d2f6da28d28e59217a3428b9df18
Oussama <oussama.aitmi@gmail.com>
2020-06-10 23:33:22 +0100

    change Directory

---
+++ tests/DataFixtures/FakerTrait.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Tests\DataFixtures;
+
+use Faker\Factory;
+use Faker\Generator;
+
+trait FakerTrait
+{
+    /**
+     * @param string $local
+     *
+     * @return Generator
+     */
+    public function getFaker($local = 'be_FR'): Generator
+    {
+        return Factory::create($local);
+    }
+
+    /**
+     * @return array
+     */
+    public function getProductFaker()
+    {
+        $faker = $this->getFaker();
+
+        return [
+            'name'                  => $faker->text(50),
+            'reference'             => $faker->randomNumber($nbDigits = NULL, $strict = false),
+            'smallDescription'     => $faker->text(150),
+            'status'                => $faker->randomElement(['DRAFT', 'PUBLISHED', 'DELETED']),
+            'statusStore'          => $faker->boolean(25),
+            'statusSiteWeb'       => $faker->boolean(25),
+            'basePrice'            => $faker->numberBetween(2, 9999),
+            'sellPrice'            => $faker->numberBetween(12, 999999),
+            'minimumSalesQuantity'=> $faker->randomNumber(2),
+            'maximumSalesQuantity'=> $faker->randomNumber(3),
+            'quantity'              => $faker->randomNumber(5),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getCategoryFaker()
+    {
+        $faker = $this->getFaker();
+        return [
+            'name'    => $faker->text(10),
+        ];
+    }
+}
