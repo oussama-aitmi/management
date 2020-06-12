ab14bbf0e051a2c8cd52ec554df40f78ec91c049
Oussama <oussama.aitmi@gmail.com>
2020-06-12 15:57:17 +0100

    desable Validator Temporary

--- src/Validator/CategoryValidator.php
+++ src/Validator/CategoryValidator.php
@@ -10,37 +10,18 @@ use App\Entity\Category;
 
 class CategoryValidator extends ConstraintValidator
 {
-    private $em;
-
-    public function __construct(EntityManagerInterface $em)
-    {
-        $this->em = $em;
-    }
-
     /**
      * @param mixed      $value
      * @param Constraint $constraint
      */
     public function validate($value, Constraint $constraint)
     {
-        $this->validateCategory($value);
+        #$this->validateCategory($value);
+        #TODO validate parentId
     }
 
     protected function validateCategory(Category $category)
     {
-        //dd("validateCategory");
-        return "dddddd";
-        /*if ( $this->isNewEntity( $category->getParent() ) ) {
-            $this->context->buildViolation('The Category does not exist-Validator')
-                ->atPath('category')
-                ->setInvalidValue(null)
-                ->addViolation();
-        }*/
-    }
-
-    private function isNewEntity($entity)
-    {
-        return \Doctrine\ORM\UnitOfWork::STATE_NEW == $this->em->getUnitOfWork()->getEntityState($entity);
     }
 
 }
