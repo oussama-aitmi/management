402f135a7b7b672612b29569e37d5efda34369c7
Oussama <oussama.aitmi@gmail.com>
2020-06-12 12:56:11 +0100

    Product Controller Test

---
+++ tests/Controller/ProductControllerTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\DataFixtures\CategoryFixtures;
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductControllerTest extends AbstractControllerTest
+{
+    protected $url = '/api/product';
+
+    public function testCreateAction()
+    {
+        $this->loadFixtures([CategoryFixtures::class]);
+        $category = self::$container->get(CategoryRepository::class)->find(1);
+
+        $data = $this->getProductFaker();
+        $data['category'] = $category->getId();
+
+        $this->sendRequest('POST', '', $data);
+        $result = $this->getDecodedResult();
+
+        $this->basicAssertions($result, Response::HTTP_CREATED);
+        $this->assertNotEmpty($result['id']);
+        $this->assertNotEmpty($result['user']['id']);
+        $this->assertNotEmpty($result['category']['id']);
+    }
+
+    public function testFailureCreateAction()
+    {
+        $data = $this->getProductFaker();
+        $data['category'] = null;
+
+        $this->sendRequest('POST', '', $data);
+        $result = $this->getDecodedResult();
+
+        $this->basicAssertions($result, Response::HTTP_BAD_REQUEST);
+    }
+
+    public function testCreateActionWithVariations()
+    {
+        $this->loadFixtures([CategoryFixtures::class]);
+        $category = self::$container->get(CategoryRepository::class)->find(1);
+
+        $data = $this->getProductFaker();
+        $data['category'] = $category->getId();
+        $data['variations'] = $this->getVariationsFaker();
+
+        $this->sendRequest('POST', '', $data);
+        $result = $this->getDecodedResult();
+
+        $this->basicAssertions($result, Response::HTTP_CREATED);
+        $this->assertNotEmpty($result['id']);
+        $this->assertEquals($result['variations'][0]['value'], $data['variations'][0]['value']);
+        $this->assertEquals($result['variations'][0]['basePrice'], $data['variations'][0]['basePrice']);
+        $this->assertEquals($result['variations'][1]['value'], $data['variations'][1]['value']);
+    }
+
+    public function testCreateActionWithImages()
+    {
+        $this->loadFixtures([CategoryFixtures::class]);
+        $category = self::$container->get(CategoryRepository::class)->find(1);
+
+        $data = $this->getProductFaker();
+        $data['category'] = $category->getId();
+
+        $this->sendRequest('POST', '', $data);
+        $result = $this->getDecodedResult();
+
+        $this->basicAssertions($result, Response::HTTP_CREATED);
+        $this->assertNotEmpty($result['id']);
+    }
+}
